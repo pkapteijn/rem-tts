4722e78ee9ab3b41a975ead62fa707337b3df31b
pkapteijn <paul.kapteijn@gmail.com>
2024-09-19 11:28:02 +0200

    serve static fe enhancements

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SentencesModule } from './sentences/sentences.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [SentencesModule],
+  imports: [    
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..',  'frontend'),
+      serveRoot: '/frontend/'
+    }),
+    SentencesModule],
   controllers: [AppController],
   providers: [AppService],
 })
